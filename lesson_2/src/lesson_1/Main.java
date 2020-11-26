f71f17026afc1c5ec3a324f8f6cb2f85e22e1a9a
NikolaySilin <silinnikolay@yandex.ru>
2020-11-26 23:46:07 +0500

    Переменные и их типы.

---
+++ lesson_2/src/lesson_1/Main.java
@@ -0,0 +1,33 @@
+package lesson_1;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+// Создайте строковую переменную со значением: «53.5»
+// Преобразуйте эту строковую переменную в переменную типа double
+// Выведите получившуюся переменную в консоль
+
+		String str = "53.5";
+		double sum = Double.parseDouble(str);
+		System.out.println(sum);
+
+//Создайте переменную типа char, в которую поместите первую букву Вашего имени
+//Выведите её в консоль.
+
+		char c = 'N';
+		System.out.println(c);
+
+// Создайте целую переменную с именем age и присвойте ей значение Вашего
+// возраста.
+// Выведите её в консоль
+
+		int age = 26;
+		System.out.println(age);
+
+
+
+
+	}
+
+}
